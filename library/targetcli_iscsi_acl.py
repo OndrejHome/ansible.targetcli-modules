c670ea5926a43a13912696c34898e0e7b075cadd
Ondrej Famera <ondrej@famera.cz>
2017-02-15 22:46:06 +0100

    targetcli_iscsi_acl improve calls and fix documentation

--- library/targetcli_iscsi_acl.py
+++ library/targetcli_iscsi_acl.py
@@ -6,7 +6,7 @@ module: targetcli_iscsi_acl
 short_description: TargetCLI iSCSI ACL module
 description:
      - module for handling iSCSI ACL objects in targetcli ('/iscsi/.../tpg1/acls').
-version_added: "0.1"
+version_added: "2.0"
 options:
   wwn:
     description:
@@ -22,6 +22,7 @@ options:
     description:
       - Should the object be present or absent from TargetCLI configuration
     required: false
+    default: present
     choices: [present, absent]
 notes:
    - Tested on CentOS 7.2
@@ -37,7 +38,7 @@ remove iSCSI ACL
 - targetcli_iscsi_acl: wwn=iqn.1994-05.com.redhat:data initiator_wwn=iqn.1994-05.com.redhat:client1 state=absent
 '''
 
-from subprocess import call
+from distutils.spawn import find_executable
 
 def main():
         module = AnsibleModule(
@@ -49,22 +50,23 @@ def main():
                 supports_check_mode=True
         )
 
-        wwn = module.params['wwn']
-        initiator_wwn = module.params['initiator_wwn']
         state = module.params['state']
+
+        if find_executable('targetcli') is None:
+            module.fail_json(msg="'targetcli' executable not found. Install 'targetcli'.")
 
         result = {}
         
         try:
-            retcode = subprocess.call("targetcli '/iscsi/" + wwn + "/tpg1/acls/" + initiator_wwn + " status'", shell=True) #FIXME think of something better than shell=True
-            if retcode == 0 and state == 'present':
+            rc, out, err = module.run_command("targetcli '/iscsi/%(wwn)s/tpg1/acls/%(initiator_wwn)s status'" % module.params)
+            if rc == 0 and state == 'present':
                 result['changed'] = False
-            elif retcode == 0 and state == 'absent':
+            elif rc == 0 and state == 'absent':
                 if module.check_mode:
                     module.exit_json(changed=True)
                 else:
-                    retcode = call("targetcli '/iscsi/" + wwn + "/tpg1/acls delete " + initiator_wwn + "'", shell=True) #FIXME think of something better than shell=True
-                    if retcode == 0:
+                    rc, out, err = module.run_command("targetcli '/iscsi/%(wwn)s/tpg1/acls delete %(initiator_wwn)s'" % module.params)
+                    if rc == 0:
                         module.exit_json(changed=True)
                     else:
                         module.fail_json(msg="Failed to delete iSCSI ACL object")
@@ -74,8 +76,8 @@ def main():
                 if module.check_mode:
                     module.exit_json(changed=True)
                 else:
-                    retcode = call("targetcli '/iscsi/" + wwn + "/tpg1/acls create " + initiator_wwn + "'", shell=True) #FIXME think of something better than shell=True
-                    if retcode == 0:
+                    rc, out, err = module.run_command("targetcli '/iscsi/%(wwn)s/tpg1/acls create %(initiator_wwn)s'" % module.params)
+                    if rc == 0:
                         module.exit_json(changed=True)
                     else:
                         module.fail_json(msg="Failed to define iSCSI ACL object")
